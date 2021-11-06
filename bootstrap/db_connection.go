d01d27fc5bf06d76cbe485df25becd735260fa84
gumball <goanglehuy185359@gmail.com>
2021-11-06 11:42:53 +0700

    fix code

--- bootstrap/db_connection.go
+++ bootstrap/db_connection.go
@@ -9,10 +9,9 @@ import (
 
 func newConnection(dbDriver, dbUser, dbPassword, dbPort, dbHost, dbName string) *sql.DB {
 	DbUrl := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", dbUser, dbPassword, dbHost, dbPort, dbName)
-
 	conn, err := sql.Open(dbDriver, DbUrl)
 	if err != nil {
-		log.Fatalf("[Can not connect to driver: %s, database %s]: %s\n", dbDriver, dbName, err)
+		log.Fatalf("Can not connect to driver: [%s], database: [%s], err: [%s]\n", dbDriver, dbName, err)
 	} else {
 		log.Printf("Connected to driver: %s, database: %s\n", dbDriver, dbName)
 	}
