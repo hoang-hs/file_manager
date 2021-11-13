c2a5ed37072aa3179aeb352b38ecc43f4437b69c
gumball <goanglehuy185359@gmail.com>
2021-11-13 11:58:57 +0700

    using viper to load file time var is not handle

--- configs/config.go
+++ configs/config.go
@@ -1,29 +1,32 @@
 package configs
 
-import "time"
+import (
+	"github.com/spf13/viper"
+	"time"
+)
 
 type Config struct {
-	AppEnv string
+	AppEnv string `mapstructure:"MODE"`
 
-	ServerAddress string
+	ServerAddress string `mapstructure:"SERVER_ADDR"`
 
-	DbDriver   string
-	DbUser     string
-	DbPassword string
-	DbPort     string
-	DbHost     string
-	DbName     string
+	DbDriver   string `mapstructure:"DB_DRIVER"`
+	DbUser     string `mapstructure:"DB_USER"`
+	DbPassword string `mapstructure:"DB_PASS"`
+	DbPort     string `mapstructure:"DB_PORT"`
+	DbHost     string `mapstructure:"DB_HOST"`
+	DbName     string `mapstructure:"DB_NAME"`
 
-	SecretKey string
+	SecretKey string `mapstructure:"SECRET_KEY"`
 
-	Root string
+	Root string `mapstructure:"ROOT"`
 
-	ExpiredDuration time.Duration
+	ExpiredDuration time.Duration `mapstructure:"EXPIRED_DURATION"`
 
-	ExpCacheTimeDb time.Duration
+	ExpCacheTimeDb time.Duration `mapstructure:"CACHE_TIME_DB"`
 
-	TelegramBotToken string
-	TelegramChatID   string
+	TelegramBotToken string `mapstructure:"FILE_MANAGER_TELEGRAM_BOT_TOKEN"`
+	TelegramChatID   string `mapstructure:"FILE_MANAGER_TELEGRAM_CHAT_ID"`
 }
 
 var Common *Config
@@ -33,37 +36,69 @@ func Get() *Config {
 }
 
 func LoadConfigs(mode string) {
-	var pathConfig string
-	switch mode {
-	case "dev":
-		pathConfig = `.env.dev`
-	case "prod":
-		pathConfig = `.env.prod`
-	default:
-		pathConfig = `.env.dev`
+	viper.SetConfigType("env")
+	//viper.AddConfigPath("")
+	viper.SetConfigFile(".env.dev")
+
+	//var configName string
+	// load env from cmd
+	viper.AutomaticEnv()
+
+	if mode == "dev" {
+		//	configName = "dev"
+	} else if mode == "prod" {
+		//	configName = "prod"
+	} else {
+		//	configName = "dev"
 	}
-	LoadEnv(pathConfig)
-	Common = &Config{
-		AppEnv: mode,
+	//viper.SetConfigName("")
 
-		ServerAddress: getStringD("SERVER_ADDR", "0.0.0.0:8080"),
-
-		DbDriver:   getString("DB_DRIVER"),
-		DbUser:     getString("DB_USER"),
-		DbPassword: getString("DB_PASS"),
-		DbPort:     getString("DB_PORT"),
-		DbHost:     getString("DB_HOST"),
-		DbName:     getString("DB_NAME"),
-
-		SecretKey: getString("SECRET_KEY"),
-
-		Root: getStringD("ROOT", "/home/gumball/"),
-
-		ExpiredDuration: getTimeDurationWithDefault("EXPIRED_DURATION", 15),
-
-		ExpCacheTimeDb: getTimeDurationWithDefault("CACHE_TIME_DB", 60),
-
-		TelegramBotToken: getString("FILE_MANAGER_TELEGRAM_BOT_TOKEN"),
-		TelegramChatID:   getString("FILE_MANAGER_TELEGRAM_CHAT_ID"),
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(err)
 	}
+
+	err = viper.Unmarshal(&Common)
+	if err != nil {
+		panic(err)
+	}
+	//viper.SetDefault("SERVER_ADDR", "0.0.0.0:8080")
+	Common.ExpCacheTimeDb *= time.Minute
+	Common.ExpiredDuration *= time.Minute
+	return
+	/*
+		var pathConfig string
+		switch mode {
+		case "dev":
+			pathConfig = `.env.dev`
+		case "prod":
+			pathConfig = `.env.prod`
+		default:
+			pathConfig = `.env.dev`
+		}
+		LoadEnv(pathConfig)
+		Common = &Config{
+			AppEnv: mode,
+
+			ServerAddress: getStringD("SERVER_ADDR", "0.0.0.0:8080"),
+
+			DbDriver:   getString("DB_DRIVER"),
+			DbUser:     getString("DB_USER"),
+			DbPassword: getString("DB_PASS"),
+			DbPort:     getString("DB_PORT"),
+			DbHost:     getString("DB_HOST"),
+			DbName:     getString("DB_NAME"),
+
+			SecretKey: getString("SECRET_KEY"),
+
+			Root: getStringD("ROOT", "/home/gumball/"),
+
+			ExpiredDuration: getTimeDurationWithDefault("EXPIRED_DURATION", 15),
+
+			ExpCacheTimeDb: getTimeDurationWithDefault("CACHE_TIME_DB", 60),
+
+			TelegramBotToken: getString("FILE_MANAGER_TELEGRAM_BOT_TOKEN"),
+			TelegramChatID:   getString("FILE_MANAGER_TELEGRAM_CHAT_ID"),
+		}
+	*/
 }
--- go.mod
+++ go.mod
@@ -6,15 +6,16 @@ require (
 	github.com/andybalholm/brotli v1.0.3 // indirect
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/gin-gonic/gin v1.7.3
-	github.com/go-playground/validator/v10 v10.9.0 // indirect
+	github.com/go-playground/validator/v10 v10.9.0
 	github.com/go-sql-driver/mysql v1.6.0
 	github.com/google/uuid v1.3.0
 	github.com/joho/godotenv v1.3.0
 	github.com/klauspost/compress v1.13.6 // indirect
 	github.com/patrickmn/go-cache v2.1.0+incompatible
-	github.com/valyala/fasthttp v1.31.0 // indirect
+	github.com/spf13/viper v1.9.0
+	github.com/valyala/fasthttp v1.31.0
 	go.uber.org/fx v1.14.2
-	go.uber.org/zap v1.16.0
-	golang.org/x/crypto v0.0.0-20210711020723-a769d52b0f97
-	gopkg.in/natefinch/lumberjack.v2 v2.0.0 // indirect
+	go.uber.org/zap v1.17.0
+	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5
+	gopkg.in/natefinch/lumberjack.v2 v2.0.0
 )
